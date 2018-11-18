2850364898442cfa8dba2f491b35c9ec1c0b2705
Senqi Cao <scao16@binghamton.edu>
2018-11-17 23:27:47 -0500

    test

--- test.c
+++ test.c
@@ -50,61 +50,7 @@ int main(){
   printf("hello%d\n",t);
   t++;
   printf("hello1%d\n",t);
-
+  
+  free(t);
   free(players);
-  
-  char * team_names[] = {
-    "team1",
-    "team2",
-    "team3",
-    "team4",
-    "team5",
-    "team6",
-    "team7",
-    "team8",
-    "team9",
-    "team10",
-    "team11",
-    "team12",
-    "team13",
-    "team14",
-    "team15",
-    "team16",
-    "team17",
-    "team18",
-    "team19",
-    "team20",
-    "team21",
-    "team22",
-    "team23",
-    "team24",
-    "team25",
-    "team26",
-    "team27",
-    "team28",
-    "team29",
-    "team30",
-    "team31",
-    "team32",
-  };
-  
-  /********************************************************************/
-  /*    DO NOT ALTER ANY OF THE THE DRIVER CODE BELOW                 */
-  /********************************************************************/
-#if !DEBUG
-  fclose(stderr);
-#endif
-  printf("\t-----------------------------------\n");
-  printf("\t-   Part1 : Creating Your League  -\n");
-  printf("\t-----------------------------------\n");
-  srand(time(NULL));
-  
-  printf("\n\t=========Test #1: Creating your Teams and Players===========\n\n");
-  Team ** league = malloc(sizeof(Team*) * NUM_TEAMS);
-  //create teams i.e. league of N teams.
-  for(int counter = 0; counter < NUM_TEAMS; counter++){
-    Player * draft = draftPlayers("players.dat", counter, NUM_PLAYERS);
-    for(int i = 0; i < NUM_PLAYERS; i++)
-      assert(draft[0].team == counter%NUM_TEAMS);
-}
 }
