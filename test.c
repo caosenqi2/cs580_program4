f3e15fa8b602aac9171af594a3be875dd70a131a
Senqi Cao <scao16@binghamton.edu>
2018-11-18 20:25:26 -0500

    test

--- test.c
+++ test.c
@@ -10,7 +10,7 @@
 #define NUM_TEAMS 32
 #define NUM_PLAYERS 10
 
-Player players[10];
+Player players[NUM_PLAYERS];
 
 Player * draftPlayers(char * filename, int team, int num_players){
   FILE * fp = fopen(filename, "r");
@@ -62,21 +62,48 @@ Player * draftPlayers(char * filename, int team, int num_players){
   return players;
 }
 
-void deleteTeam(Team * t){
-  free(t->name);
-  free(t->players);
-  free(t);
+void deleteTeam(Team * team){
+  free(team->name);
+  free(team->players);
+  free(team);
 };
 
 Team * newTeam(char * name, Player * players){
-  Team * t = malloc(sizeof(Team));
-  strcpy(t->name,name);
-  t->players = players;
-  t->delete = deleteTeam;
-  return t;
+  Team * team = malloc(sizeof(Team));
+  strcpy(team->name,name);
+  team->players = players;
+  team->delete = deleteTeam;
+  return team;
 }
 
+Team * game(Team * team1, Team * team2){
+  if (team1 == NULL || team2 == NULL) return NULL;
+  int i = 0;
+  int deffensive_1 = 0;
+  int deffensive_2 = 0;
+  int offensive_1 = 0;
+  int offensive_2 = 0;
+  int score_1 = 0;
+  int score_2 = 0;
+  
+  for (i=0;i<10;i++){
+    printf("offensives:%d\n",players[i].offensive);
+    deffensive_1 += team1->players[i].defensive;
+    deffensive_2 += team2->players[i].defensive;
+    offensive_1 += team1->players[i].offensive;
+    offensive_2 += team2->players[i].offensive;
+  }
+  while(score_1 == score_2){
+    int s1 = rand() % (offensive_1 + 1);
 
+    if (rand() % (offensive_1 + 1) > deffensive_2) score_1+=1;
+    if (rand() % (offensive_1 + 1) > deffensive_2) score_1+=1;
+    if (rand() % (offensive_2 + 1) > deffensive_1) score_2+=1;
+    if (rand() % (offensive_2 + 1) > deffensive_1) score_2+=1;
+    if (score_1 > score_2) return team1;
+    if (score_2 > score_1) return team1;
+  }
+};
 
 int main(){
   FILE * fp = fopen("players.dat", "r");
@@ -186,5 +213,28 @@ int main(){
     assert((league[counter]->players[rand() % NUM_PLAYERS]).defensive > 0);
     assert(sizeof(*(league[counter])) == sizeof(Team));
   }
+  printf("\t-----------------------------------\n");
+  printf("\t-   Part2 : Exhibition Games      -\n");
+  printf("\t-----------------------------------\n");
   
+  printf("\n\t=========Test #2: Sanity check for the game===========\n\n");
+  
+  Team * winner = game(NULL, NULL);
+  assert(winner == NULL);
+  printf("\n\t\t....Test Passed\n");
+  printf("\n\t=========Test #3: Playing a Game between two random teams===========\n\n");
+  Team * team1 = league[rand() % NUM_TEAMS];
+  Team * team2 = league[rand() % NUM_TEAMS];
+  printf("Up next, an exhibition game between %s and %s\n", team1->name, team2->name);
+  winner = game(team1, team2);
+  assert(winner == team1 || winner == team2);
+  printf("The winning team is %s\n\n", winner->name);
+  printf("\n\t\t....Test Passed\n");
+  printf("\n\t=========Test #4: Playing a Game between the same team===========\n\n");
+  int team_num = rand() % NUM_TEAMS;
+  printf("Up next, a a scrimmage for %s\n", league[team_num]->name);
+  winner = game(league[team_num], league[team_num]);
+  assert(winner == league[team_num]);
+  printf("The winning team is %s\n\n", winner->name);
+  printf("\n\t\t....Test Passed\n");
 }
--- tournament.c
+++ tournament.c
@@ -3,59 +3,74 @@
 #include <string.h> 
 #include "tournament.h"
 
+#define NUM_PLAYERS 10
+Player players[NUM_PLAYERS];
+
 Player * draftPlayers(char * filename, int team, int num_players){
   FILE * fp = fopen(filename, "r");
   
   char * line = NULL;
   size_t len = 0;
   ssize_t read;
-  Player players[num_players];
-  
+  //Player players[num_players];
+  //Player * players = malloc(num_players*sizeof(Player));
+  int i = 0;
   while ((read = getline(&line, &len, fp)) != -1) {
+    //printf("%s\n",line);
     char *token = strtok(line, ",");
     
     if (atoi(token) == team){
-      players[0].team = atoi(token);
+      players[i].team = atoi(token);
       token = strtok(NULL, ",");
       
-      players[1].first = token;
+      strcpy(players[i].first,token);
       token = strtok(NULL, ",");
       
-      players[2].last = token;
+      strcpy(players[i].last,token);
       token = strtok(NULL, ",");
       
-      players[3].number = atoi(token);
+      players[i].number = atoi(token);
       token = strtok(NULL, ",");
       
-      players[4].offensive = atoi(token);
+      players[i].offensive = atoi(token);
       token = strtok(NULL, ",");
       
-      players[5].defensive = atoi(token);
+      players[i].defensive = atoi(token);
       token = strtok(NULL, ",");
+      i++;
+      //printf("team:%d\n", players[0].team);
+      //printf("first:%s\n", players[1].first);
+      //printf("last:%s\n", players[2].last);
+      //printf("num:%d\n", players[3].number);
+      //printf("off:%d\n", players[4].offensive);
+      //printf("deff:%d\n", players[5].defensive);
     }
   }
   
   fclose(fp);
   if (line)
     free(line);
+  //free(players);
   return players;
 }
 
-void deleteTeam(Team * t){
-  free(t->name);
-  free(t->players);
-  free(t);
+void deleteTeam(Team * team){
+  free(team->name);
+  free(team->players);
+  free(team);
 };
 
 Team * newTeam(char * name, Player * players){
-  Team * t = malloc(sizeof(Team));
-  strcpy(t->name,name);
-  t->players = players;
-  t->delete = deleteTeam;
-  return t;
+  Team * team = malloc(sizeof(Team));
+  strcpy(team->name,name);
+  team->players = players;
+  team->delete = deleteTeam;
+  return team;
 }
 
-Team * game(Team *, Team *);
+Team * game(Team * team1, Team * team2){
+  
+};
 
 Team * tournament(Team **, int);
 
